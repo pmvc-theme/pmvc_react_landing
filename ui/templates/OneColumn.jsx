c24cb8dfa35ed7f7dbf7f0e20c7814e1497fb8a3
Hill <hill@kimo.com>
2016-11-05 18:19:35 +0000

    improve template

--- ui/templates/OneColumn.jsx
+++ ui/templates/OneColumn.jsx
@@ -1,12 +1,33 @@
 import React from 'react'; 
+import { ContentWrapper } from '../../src/index';
 
-const OneColumn = (props) => (
+const OneColumn = (props) => {
+    let {
+        header,
+        menu,
+        body,
+        footer,
+        cover,
+        contentWrapper
+    } = props;
+    if (cover) {
+        body = ( 
+            <ContentWrapper {...contentWrapper}>
+                {body}
+                {footer}
+            </ContentWrapper>
+        );
+        footer = null;
+    }
+    return (
     <div>
-        {props.header}
-        {props.menu}
-        {props.body}
-        {props.footer}
+        {header}
+        {menu}
+        {cover}
+        {body}
+        {footer}
     </div>
-);
+    );
+};
 
 export default OneColumn;
