689c8296849af44eac79e4bba80118a4450d4652
Hill <hill@kimo.com>
2017-09-21 18:33:41 +0000

    update for use prect

---
+++ src/server.js
@@ -0,0 +1,4 @@
+import server from 'reshow-app/server';
+import app from '../ui/pages/index';
+
+global.app=server(app);
---
+++ webpack.server.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const webpack = require('reshow-app/webpack.server');
+
+const c = webpack(__dirname);
+module.exports = c;
