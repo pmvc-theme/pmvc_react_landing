60a2adcc06f2de95954f9b287d6b9d69571bdf26
Hill <hill@kimo.com>
2018-07-05 01:37:45 +0800

    Add splashStyle

--- ui/molecules/SplashBlock.jsx
+++ ui/molecules/SplashBlock.jsx
@@ -1,17 +1,17 @@
 import React, {Children, cloneElement} from 'react'; 
 import { SemanticUI } from 'react-atomic-molecule';
 
-const SplashBlock = (props) => (
+const SplashBlock = ({children, style, splashStyle, ...props}) => (
     <SemanticUI
         atom="section"
         className="splash-block"
         {...props}
-        style={{...Styles.block, ...props.style}}
+        style={{...Styles.block, ...style}}
     >
         <SemanticUI
             className="splash"
-            style={Styles.insideContainer}>
-            {Children.map( props.children, (child, key) => cloneElement(child, {key}))}
+            style={{...Styles.insideContainer, ...splashStyle}}>
+            {Children.map( children, (child, key) => cloneElement(child, {key}))}
         </SemanticUI>
     </SemanticUI>
 );
